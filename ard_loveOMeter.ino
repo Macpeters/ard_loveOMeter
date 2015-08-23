09a2fc6fde2b3cf0c0fbc32f6835e1f7ca4dd3c0
Macpeters <peters.maryanne@gmail.com>
2015-08-23 09:20:19 -0400

    finished exercise

--- ard_loveOMeter.ino
+++ ard_loveOMeter.ino
@@ -10,7 +10,7 @@ void setup() {
   }
 }
 
-void loop() {
+void loop() { 
   int sensorVal = analogRead(sensorPin);
   Serial.print("Sensor Value: ");
   Serial.print(sensorVal);
@@ -19,4 +19,31 @@ void loop() {
   float voltage = (sensorVal/1024.0) * 5.0;
   Serial.print(", Volts: ");
   Serial.print(voltage);
+  
+  Serial.print(", degrees C: ");
+  float temperature = (voltage - 0.5) * 100;
+  Serial.println(temperature);
+
+  if(temperature < baselineTemp){
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW); 
+  }else if(temperature >= baselineTemp+2 &&
+              temperature < baselineTemp+4){
+     digitalWrite(2, HIGH);
+     digitalWrite(3, LOW);
+     digitalWrite(4, LOW);     
+  }else if(temperature >= baselineTemp+4 &&
+              temperature < baselineTemp+6){
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);              
+  }else if(temperature >= baselineTemp+6){
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  }
+  delay(1);
+  
+  
 }
